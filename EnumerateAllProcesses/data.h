f9cb07f321d540aba3044e40389cf475d8e97a16
moffern <73747749+moffern@users.noreply.github.com>
2022-06-24 16:34:43 +0200

    Add files via upload

---
+++ EnumerateAllProcesses/data.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "fastmutex.h"
+
+
+#define DbgMsg(x, ...) DbgPrintEx(0, 0, x, __VA_ARGS__)
+#define DRIVER_TAG 'lifo'
+
+UNICODE_STRING dos = RTL_CONSTANT_STRING(L"\\??\\random");
+
+NTSTATUS IrpComplete(PIRP Irp, NTSTATUS Status = STATUS_SUCCESS, ULONG_PTR Info = 0);
+DRIVER_UNLOAD UnloadDriver;
+DRIVER_DISPATCH CreateClose, IoControl;
+
+int ref_index{};
+ProcessInfo* RetProcByName(const char* name, vector<ProcessInfo*>& vec, int& index = ref_index);
+void FindProcess(const char* name, vector<ProcessInfo*>& vec);
+bool FindPid(ULONG pid, ProcessInfo* process = nullptr);
+bool RemovePid(ULONG pid, ProcessInfo* proc);
+void HideByPid(ULONG pid);
+
+void OnProcessNotify(PEPROCESS, HANDLE ProcessId, PPS_CREATE_NOTIFY_INFO CreateInfo);
+
+constexpr auto SizeOf = []<size_t size>(auto(&)[size]) { return size; };
+#define SIZEOF(a) SizeOf(a)
