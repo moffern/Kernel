38529423c5bcf3cb84e0d3f16bcf20e0df6d3c0a
moffern <73747749+moffern@users.noreply.github.com>
2022-07-26 01:15:51 +0200

    Update Alloc.h

--- CustomFuncs/Alloc.h
+++ CustomFuncs/Alloc.h
@@ -106,17 +106,17 @@ inline PVOID _ALLOC_::Alloc(size_t NumberOfBytes, ULONG64 PoolFlag, ULONG Tag)
 		ASSERT(KeGetCurrentIrql() <= DISPATCH_LEVEL);
 	
 	if (_capacity == 0)
-		_alloc_.Init(8);
+		Init(8);
 	else if (_size == _capacity)
-		_alloc_.Init(_capacity + 8);
+		Init(_capacity + 8);
 
 	if (is_active() && _size < _capacity)
 	{
 		auto ptr = ExAllocatePool2(PoolFlag, NumberOfBytes, Tag);
 		if (ptr)
 		{
-			AutoLock lock(_alloc_._mutex);
-			_alloc_._alloc[_alloc_._size++] = (ULONG_PTR)ptr;
+			AutoLock lock(_mutex);
+			_alloc[_size++] = (ULONG_PTR)ptr;
 			// Memory is zero initialized unless POOL_FLAG_UNINITIALIZED is specified.
 			//RtlZeroMemory(ptr, NumberOfBytes);
 			return ptr;
@@ -136,25 +136,25 @@ inline bool _ALLOC_::Free(auto& p)
 {
 	ASSERT(KeGetCurrentIrql() <= DISPATCH_LEVEL);
 
-	if (_alloc_._size == 0 || p == nullptr || !_alloc_.is_active())
+	if (_size == 0 || p == nullptr || !is_active())
 		return false;
 
 	if (_size < (_capacity / 2) && ((_capacity / 2) % 8) == 0)
-		_alloc_.Init(_capacity / 2);
+		Init(_capacity / 2);
 	else if (_size < (_capacity / 2) && ((_capacity - 8) % 8) == 0)
-		_alloc_.Init(_capacity - 8);
+		Init(_capacity - 8);
 
-	for (int i = 0; i < _alloc_._size; ++i)
+	for (int i = 0; i < _size; ++i)
 	{
-		if (_alloc_._alloc[i] == (ULONG_PTR)p)
+		if (_alloc[i] == (ULONG_PTR)p)
 		{
-			AutoLock lock(_alloc_._mutex);
-			if (i == _alloc_._size - 1)
-				_alloc_._alloc[--_alloc_._size] = NULL;
+			AutoLock lock(_mutex);
+			if (i == _size - 1)
+				_alloc[--_size] = NULL;
 			else
 			{
-				_alloc_._alloc[i] = _alloc_._alloc[_alloc_._size - 1];
-				_alloc_._alloc[--_alloc_._size] = NULL;
+				_alloc[i] = _alloc[_size - 1];
+				_alloc[--_size] = NULL;
 			}
 
 			DbgMsg("ExFreePool(%p) called\n", p);
